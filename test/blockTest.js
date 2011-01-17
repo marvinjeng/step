61f74476fd4e1026ce58397d2ef508f5537c710a
Hunter Loftis <hunter.loftis+github@gmail.com>
2011-01-17 12:04:36 -0500

    Simplified all around, made complete handler follow node callback idiom for more compatibility

--- test/blockTest.js
+++ test/blockTest.js
@@ -17,31 +17,34 @@ function something_stable(callback, val) {
   })
 }
 
-Step.block({
-  steps: [
-    function a() {
-      console.log('one')
-      this()
-    },
-    function b() {
-      console.log('two')
-      this(null, 'three')
-    },
-    function c(n) {
-      something_stable(this, n)  
-    },
-    function d(m) {
-      console.log(m)
-      return (m + ' four')
-    },
-    function e() {
-      console.log("Should never get here")
-    }
-  ],
-  success: function(val) {
-    console.log('returned: ' + val)
+Step.block(
+  function start() {
+    console.log("Starting Step.block...")
+    this()
   },
-  error: function(err) {
-    console.warn(err.stack || err)
-  }
-})
+  function a() {
+    console.log('one')
+    this()
+  },
+  function b() {
+    console.log('two')
+    this(null, 'three')
+  },
+  function c(n) {
+    something_stable(this, n)  
+  },
+  function d(m) {
+    console.log(m)
+    return (m + ' four')
+  },
+  function e() {
+    console.log("Should never get here")
+  },
+  function complete(err, val) {
+    if (!err && val) {
+      console.log('returned: ' + val)
+    }
+    else {
+      console.warn(err.stack || err)
+    }
+  })
